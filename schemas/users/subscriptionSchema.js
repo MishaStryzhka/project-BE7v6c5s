a0cb1c0dc78f844e06630340d17bb7b97c775c3f
Dimon <bolilui.dmytro@gmail.com>
2023-07-22 12:42:22 +0300

    ADDED: connection to actual database and updated schemas

--- schemas/users/subscriptionSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const subscriptionList = ["starter", "pro", "business"];
-const subscriptionSchema = Joi.object({
-    subscription: Joi.string().valid(...subscriptionList).required(),
-});
-
-module.exports = subscriptionSchema;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const mongoose = require("mongoose");
-const app = require("./app");
+const mongoose = require('mongoose');
+const app = require('./app');
 
-const { DB_HOST } = process.env;
-mongoose.set("strictQuery", true);
+// const { DB_HOST } = process.env;
+mongoose.set('strictQuery', true);
+
+const DB_HOST =
+  'mongodb+srv://project-BE7v6c5s:BE7v6c5s@cluster0.u2sazxy.mongodb.net/PetsShop?retryWrites=true&w=majority';
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connection successful");
+    console.log('Database connection successful');
     app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log('Server running. Use our API on port: 3000');
     });
   })
   .catch((error) => {
