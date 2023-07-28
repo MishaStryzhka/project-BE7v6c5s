c0571b40219e0f8bdfcfa839cbaca8a1623b76fb
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-07-28 17:40:44 +0200

    Update getNews.js
    Adds pagination

--- controllers/news/getNews.js
+++ controllers/news/getNews.js
@@ -1,8 +1,8 @@
 const News = require("../../models/news");
 
 const getNews = async (req, res, next) => {
-    const news = await News.find();
-    console.log("news", news);
+    const { page } = req.body
+    const news = await News.find().skip(6*(page-1)).limit(6);
 
     res.status(200).json(news);
 };
