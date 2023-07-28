59276ad48e796ea2c7b243ce5b086f905728b472
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-07-28 19:18:50 +0200

    Update getNews.js
    Adds quantity news

--- controllers/news/getNews.js
+++ controllers/news/getNews.js
@@ -3,8 +3,9 @@ const News = require("../../models/news");
 const getNews = async (req, res, next) => {
     const { page } = req.body
     const news = await News.find().skip(6*(page-1)).limit(6);
+    const quantityNews = await News.find().count();
 
-    res.status(200).json(news);
+    res.status(200).json({quantityNews , news});
 };
 
 module.exports = getNews;
