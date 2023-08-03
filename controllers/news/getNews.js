a8d81beecac693aed1c5f04a3610bfd2255383c0
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-08-03 04:13:23 +0200

    Update getNews.js

--- controllers/news/getNews.js
+++ controllers/news/getNews.js
@@ -1,17 +1,17 @@
 const News = require("../../models/news");
 
 const getNews = async (req, res, next) => {
-    const { page, limit, search } = req.query;
+    const { page, limit, query } = req.query;
     console.log("req", req.query);
 
     const news = await News.find({
-        title: { $regex: search, $options: "i" },
+        title: { $regex: query, $options: "i" },
     })
         .skip(limit * (page - 1))
         .limit(limit);
 
     const quantityNews = await News.find({
-        title: { $regex: search, $options: "i" },
+        title: { $regex: query, $options: "i" },
     });
 
     res.status(200).json({ quantityNews: quantityNews.length, news });
