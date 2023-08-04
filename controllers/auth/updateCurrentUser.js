d14e62fe0cff2bb54e6dafb8dbf1e832e48f51b9
Dimon <bolilui.dmytro@gmail.com>
2023-08-04 09:06:28 +0300

    Add favorite to updateUser

--- controllers/auth/updateCurrentUser.js
+++ controllers/auth/updateCurrentUser.js
@@ -1,73 +1,75 @@
-const { HttpError } = require("../../helpers");
-const { User } = require("../../models");
+const { HttpError } = require('../../helpers');
+const { User } = require('../../models');
 
 const updateCurrentUser = async (req, res, next) => {
-    const { _id } = req.user;
-    const { name, email, birthday, phone, city } = req.body;
+  const { _id } = req.user;
+  const { name, email, birthday, phone, city } = req.body;
 
-    const user = await User.findById(_id);
-    if (!user) {
-        next(HttpError(401, "Not authorized"));
-    }
+  const user = await User.findById(_id);
+  if (!user) {
+    next(HttpError(401, 'Not authorized'));
+  }
 
-    const isNewEmailExist = await User.findOne({ email });
+  const isNewEmailExist = await User.findOne({ email });
 
-    if (isNewEmailExist) {
-        throw HttpError(409, "Email in use");
-    }
+  if (isNewEmailExist) {
+    throw HttpError(409, 'Email in use');
+  }
 
-    if (req.file) {
-        const updatedUser = await User.findByIdAndUpdate(
-            _id,
-            {
-                name,
-                email,
-                birthday,
-                phone,
-                city,
-                avatar: req.file.path,
-            },
-            {
-                new: true,
-            }
-        );
+  if (req.file) {
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        name,
+        email,
+        birthday,
+        phone,
+        city,
+        avatar: req.file.path,
+      },
+      {
+        new: true,
+      }
+    );
 
-        res.status(200).json({
-            User: {
-                name: updatedUser.name,
-                email: updatedUser.email,
-                birthday: updatedUser.birthday,
-                phone: updatedUser.phone,
-                city: updatedUser.city,
-                avatar: updatedUser.avatar,
-            },
-        });
-    } else {
-        const updatedUser = await User.findByIdAndUpdate(
-            _id,
-            {
-                name,
-                email,
-                birthday,
-                phone,
-                city,
-            },
-            {
-                new: true,
-            }
-        );
+    res.status(200).json({
+      User: {
+        name: updatedUser.name,
+        email: updatedUser.email,
+        birthday: updatedUser.birthday,
+        phone: updatedUser.phone,
+        city: updatedUser.city,
+        avatar: updatedUser.avatar,
+        favorite: updatedUser.favorite,
+      },
+    });
+  } else {
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        name,
+        email,
+        birthday,
+        phone,
+        city,
+      },
+      {
+        new: true,
+      }
+    );
 
-        res.status(200).json({
-            User: {
-                name: updatedUser.name,
-                email: updatedUser.email,
-                birthday: updatedUser.birthday,
-                phone: updatedUser.phone,
-                city: updatedUser.city,
-                avatar: updatedUser.avatar,
-            },
-        });
-    }
+    res.status(200).json({
+      User: {
+        name: updatedUser.name,
+        email: updatedUser.email,
+        birthday: updatedUser.birthday,
+        phone: updatedUser.phone,
+        city: updatedUser.city,
+        avatar: updatedUser.avatar,
+        favorite: updatedUser.favorite,
+      },
+    });
+  }
 };
 
 module.exports = updateCurrentUser;
